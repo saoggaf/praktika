ab330a943441be88da86a364f5ef6024cc488d3f
Nikolay Meleshchenko <kolan0216@users.noreply.github.com>
2022-03-21 19:39:10 +0300

    feat: добавлена тема оформления

---
+++ src/Theme.java
@@ -0,0 +1,10 @@
+@Getter
+@Setter
+public class Theme {
+private String name;
+private Font font;
+private Color backgroundColor;
+private Color textColor;
+private Image backgroundImage;
+private Markup markup;
+}
